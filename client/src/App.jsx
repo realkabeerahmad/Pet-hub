c4de3947fcf26c02c0ecff9f91776734ee390bec
realkabeerahmad <kabeerahmadofficial@gmail.com>
2022-08-21 17:18:05 +0500

    Added Basic Pages to Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,30 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Navigation from "./components/Navigation/Navigation";
+import Community from "./pages/Community/Community";
+import Home from "./pages/Home/Home";
+import MyPets from "./pages/MyPets/MyPets";
+import Shop from "./pages/Shop/Shop";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="navbar">
-          <Navigation></Navigation>
-        </div>
+        <Navigation></Navigation>
         <div className="App">
-          This is Acctual App
-          <Routes></Routes>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/my_pets" element={<MyPets />}></Route>
+            <Route path="/shop" element={<Shop />}></Route>
+            <Route path="/community" element={<Community />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
         </div>
-        <div className="footer">
-          <Footer></Footer>
-        </div>
+        <Footer></Footer>
       </BrowserRouter>
     </>
   );
--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,57 @@
 import React from "react";
-
-const Navigation = () => {
-  return <div>Navigation</div>;
+import { NavLink } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+import "./Navigation.css";
+const Navigation = (user) => {
+  return (
+    <nav className="nav-bar">
+      <div className="nav-logo">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="nav">
+        <div className="nav-links">
+          {user && user._id ? (
+            <>
+              <NavLink exact to="/" activeClassName="is-active">
+                Home
+              </NavLink>
+              <NavLink to="/my_pets" activeClassName="is-active">
+                My Pets
+              </NavLink>
+              <NavLink to="/shop" activeClassName="is-active">
+                Shop
+              </NavLink>
+              <NavLink to="/adopt" activeClassName="is-active">
+                Adopt
+              </NavLink>
+              <NavLink to="/community" activeClassName="is-active">
+                Community
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink exact to="/" activeClassName="is-active">
+                Home
+              </NavLink>
+            </>
+          )}
+        </div>
+        {user && user._id ? (
+          <div className="auth_links">User</div>
+        ) : (
+          <div className="auth_links">
+            <NavLink to="/login" activeClassName="is-active">
+              Login
+            </NavLink>{" "}
+            |{" "}
+            <NavLink to="/register" activeClassName="is-active">
+              Register
+            </NavLink>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
