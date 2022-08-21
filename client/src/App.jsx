63a57d90f50e69175d971a28a90c3f6ca6c2ab7c
realkabeerahmad <kabeerahmadofficial@gmail.com>
2022-08-21 14:10:34 +0500

    Setting up Client App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-  return <div>Here is our app</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="navbar">
+          <Navigation></Navigation>
+        </div>
+        <div className="App">
+          This is Acctual App
+          <Routes></Routes>
+        </div>
+        <div className="footer">
+          <Footer></Footer>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
